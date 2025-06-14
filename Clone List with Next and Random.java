60551980bd865e0e20ffe5a538d8510de84f9da6
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-06-14 08:57:23 +0530

    Create Clone List with Next and Random.java

---
+++ Clone List with Next and Random.java
@@ -0,0 +1,48 @@
+/*linked list node
+class Node {
+    int data;
+    Node next;
+    Node random;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        random = null;
+    }
+}
+*/
+class Solution {
+    public Node cloneLinkedList(Node head) {
+        // code here
+        if (head == null) {
+            return null;
+        }
+        Node curr = head;
+        while (curr != null) {
+            Node newNode = new Node(curr.data);
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next;
+        }
+        curr = head;
+        while (curr != null) {
+            if( curr.random != null){
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+        curr = head;
+        Node cloneHead = head.next;
+        Node clone = cloneHead;
+        while (clone.next != null){
+            curr.next = curr.next.next;
+            clone.next = clone.next.next;
+            curr = curr.next;
+            clone = clone.next;
+        }
+        curr.next = null;
+        clone.next = null;
+        
+        return cloneHead;
+    }
+}
