1bff8a10a590a4e27e2d5c962b04b7b6e31e6e47
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-07-07 09:01:42 +0530

    Create k-th Smallest in BST.java

---
+++ k-th Smallest in BST.java
@@ -0,0 +1,34 @@
+// class Node
+// {
+//     int data;
+//     Node left, right;
+
+//     public Node(int d)
+//     {
+//         data = d;
+//         left = right = null;
+//     }
+// }
+
+class Solution {
+    // Return the Kth smallest element in the given BST
+    int count = 0;
+    int result = -1;
+    public int kthSmallest(Node root, int k) {
+        // Write your code here
+        inOrder(root, k);
+        return result;
+    }
+    private void inOrder(Node node, int k){
+        if (node == null){
+            return;
+        }
+        inOrder(node.left, k);
+        count++;
+        if (count == k){
+            result = node.data;
+            return;
+        }
+        inOrder(node.right, k);
+    }
+}
