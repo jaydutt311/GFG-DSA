f89be6ef1d7e0b70ba4262a9d9888d21c82710ad
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-08-05 09:36:08 +0530

    Create Min Cost Climbing Stairs.java

---
+++ Min Cost Climbing Stairs.java
@@ -0,0 +1,16 @@
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        // Write your code here
+        int n = cost.length;
+        int prev1 = cost[1];
+        int prev2 = cost[0];
+        for (int i = 2; i < n; i++) {
+            int curr = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return Math.min(prev1, prev2);
+    }
+};
