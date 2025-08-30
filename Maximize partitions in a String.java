9ecf93c968972a6b3a2ed17f6e5ccbfa480c8786
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-08-30 19:18:54 +0530

    Create Maximize partitions in a String.java

---
+++ Maximize partitions in a String.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPartitions(String s) {
+        // code here
+        int[] last = new int[26];
+    for (int i = 0; i < s.length(); i++){
+        last[s.charAt(i) - 'a'] = i;
+    }
+    int count = 0;
+    int end = 0;
+    for (int i = 0; i < s.length(); i++){
+        end = Math.max(end, last[s.charAt(i) - 'a']);
+        if (i == end) {
+            count++;
+        }
+    }
+    return count;
+  }
+}
