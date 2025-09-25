9e874a09dbe0da186726224517ae839262e3d832
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-09-25 21:27:06 +0530

    Create Maximize Number of 1's.java

---
+++ Maximize Number of 1's.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxOnes(int arr[], int k) {
+        // code here
+       int left = 0, right = 0;
+       int zeroCount = 0;
+        int maxLen = 0;
+        while (right < arr.length) {
+            if (arr[right] == 0){
+                zeroCount++;
+            }
+            while (zeroCount > k){
+                if (arr[left] == 0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+}
