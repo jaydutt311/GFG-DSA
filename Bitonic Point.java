f11aaf4bb845d5217fd0e4f1d64613d1e19ef1e6
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-10-11 20:54:55 +0530

    Create Bitonic Point.java

---
+++ Bitonic Point.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    public int findMaximum(int[] arr) {
+        // code here
+        int left  = 0;
+        int right = arr.length - 1;
+        while ( left < right ){
+            int mid = left + ( right - left ) / 2;
+            if ( arr[mid] < arr[mid + 1]){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return arr[left];
+    }
+}
