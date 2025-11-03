75b25d76202b2b634d1d26651967eac557eebb52
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-11-03 20:13:05 +0530

    Create Pairs with difference k.java

---
+++ Pairs with difference k.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    int countPairs(int[] arr, int k) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : arr){
+            if (map.containsKey(num - k))
+            count += map.get(num - k);
+            if (map.containsKey(num + k))
+            count += map.get(num + k);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+           return count;
+     }
+  }
