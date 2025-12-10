c2c9b25a7e187948e11bf2037d94fa835375e759
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2025-12-10 21:48:53 +0530

    Add solution for checking symmetric binary tree
    Implement isSymmetric method to check if a binary tree is symmetric.

---
+++ Symmetric Tree.java
@@ -0,0 +1,26 @@
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution {
+    public boolean isSymmetric(Node root) {
+        // code here
+     if (root == null) return true;
+     return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(Node left, Node right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.data != right.data)
+        return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
