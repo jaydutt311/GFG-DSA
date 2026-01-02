dc7b1fa43a0f719b75bc43abcb139eefd6413227
JAYDUTT UPADHYAY <152434199+jaydutt311@users.noreply.github.com>
2026-01-02 21:20:18 +0530

    Add function to count nodes in given range
    Implement function to count BST nodes within a range.

---
+++ Count BST nodes that lie in a given range .java
@@ -0,0 +1,30 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    int getCount(Node root, int l, int h) {
+        // Your code here
+        if (root == null) {
+            return 0;
+        }
+        if (root.data < l) {
+            return getCount(root.right, l, h);
+        }
+        if (root.data > h) {
+            return getCount(root.left, l, h);
+        }
+        return 1
+        + getCount(root.left, l, h)
+        + getCount(root.right, l, h);
+    }
+}
